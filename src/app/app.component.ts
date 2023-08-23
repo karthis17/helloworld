3a472105576cad6a5538012810e392d7164202f1
karthis17 <karthirs602@gmail.com>
2023-08-23 13:10:59 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>HelloWorld</h1>',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'helloWorld';
+  title = 'home';
 }
